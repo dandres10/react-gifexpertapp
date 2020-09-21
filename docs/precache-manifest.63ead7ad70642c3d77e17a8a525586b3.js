12f364fb4fcfd720ea8a57003742c4e061fd48dd
Andres Leon <AndresLL@digitalware.com.co>
2020-09-21 09:22:59 -0500

    carpeta docs

---
+++ docs/precache-manifest.63ead7ad70642c3d77e17a8a525586b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6f59a6a05f9c62b3e6f7e5aa2ca995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cc60e096d17a4d2c5ab",
+    "url": "/static/css/main.0c72648e.chunk.css"
+  },
+  {
+    "revision": "c891655c70870ca4f1e5",
+    "url": "/static/js/2.00eec8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eec8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc60e096d17a4d2c5ab",
+    "url": "/static/js/main.6d0798d5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
